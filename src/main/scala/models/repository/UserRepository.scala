35719b83ba1164e0895ac1befc9ad7ad59299415
ptabasso2 <pejman.tabassomi@gmail.com>
2019-02-21 19:48:27 +0100

    Initial commit

---
+++ src/main/scala/models/repository/UserRepository.scala
@@ -0,0 +1,23 @@
+package models.repository
+
+import models.User
+import org.mongodb.scala._
+import org.mongodb.scala.bson.ObjectId
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class UserRepository(collection: MongoCollection[User])(implicit ec: ExecutionContext) {
+  def findById(id: String): Future[Option[User]] =
+    collection
+      .find(Document("_id" -> new ObjectId(id)))
+      .first
+      .head
+      .map(Option(_))
+    Thread.sleep(5000)
+
+  def save(user: User): Future[String] =
+    collection
+      .insertOne(user)
+      .head
+      .map { _ => user._id.toHexString }
+}
